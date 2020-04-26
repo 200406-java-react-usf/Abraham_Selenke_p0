fe4ce061445f2de46708ff06c2ddbcb3864160d0
Abraham Selenke <slinky@Abraham.local>
2020-04-26 16:17:16 -0600

    Added comments to Balance

--- src/models/balances.ts
+++ src/models/balances.ts
@@ -8,11 +8,16 @@ export class Balance{
     userId: number;
 
     constructor(init: number, sv: number, sp: number, tot: number, request: number, uid: number){
+        //Starting amount
         this.start = init;
+        //How much money comes in and out
         this.save = sv;
         this.spent = sp;
+        //Total amount remaining
         this.total = tot;
+        //Money requested
         this.request = request;
+        //Connects the user to a balance
         this.userId = uid;
     }
 }
