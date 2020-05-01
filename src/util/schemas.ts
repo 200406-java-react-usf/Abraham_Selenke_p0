cad9ab1b4922f8997830333da4dacef024b30ea0
Abraham Selenke <slinky@Abraham.local>
2020-05-01 13:02:51 -0600

    Added UserSchema Interface

---
+++ src/util/schemas.ts
@@ -0,0 +1,10 @@
+export interface UserSchema {
+    id: number,
+    username: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    eamil: string,
+    role_name: string
+}
+
