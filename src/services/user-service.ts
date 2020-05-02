4bf2d934d40ef56fdbd427d79641b7cc24630454
Abraham Selenke <slinky@Abraham.local>
2020-05-02 10:47:47 -0600

    add logic for getUserById and getUserByUniqueKey

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,7 +1,12 @@
 import { User } from "../models/users";
 import { UserRepository } from "../repos/user-repos";
-//import { isValidId, isValidObject, isValidString } from "../util/validator"
-import { ResourceNotFoundError } from "../errors/errors"
+import { 
+    isValidId, 
+    isValidObject, 
+    isValidString,
+    isEmptyObject 
+} from "../util/validator"
+import { ResourceNotFoundError, BadRequestError } from "../errors/errors"
 
 export class UserService {
     constructor(private userRepo: UserRepository) {
@@ -21,6 +26,58 @@ export class UserService {
         }
     }
 
+    async getUserById(id: number): Promise<User> {
+        if (!isValidId(id)) {
+            throw new BadRequestError();
+        }
+
+        let user = await this.userRepo.getById(id);
+
+        if(isEmptyObject(user)) {
+            throw new ResourceNotFoundError();
+        }
+
+        return this.removePassword(user);
+    }
+
+    async getUserByUniqueKey(queryObj: any): Promise<User> {
+
+        try {
+
+            let queryKeys = Object.keys(queryObj);
+
+            if(!queryKeys.every(key => isPropertyOf(key, User))) {
+                throw new BadRequestError();
+            }
+
+            //support single param searches...can we do better?
+            let key = queryKeys[0];
+            let val = queryKeys[key];
+
+            //reuse the logic
+            if (key === 'id') {
+                return await this.getUserById(+val);
+            }
+
+            //is key valid?
+            if(!isValidString(val)){
+                throw new BadRequestError();
+            }
+
+            let user = await this.userRepo.getUserByUniqueKey(key, val);
+
+            if (isEmptyObject(user)) {
+                throw new ResourceNotFoundError();
+            }
+
+            return this.removePassword(user);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+
+    
     private removePassword(user: User): User {
         if(!user || !user.password) return user;
         let usr = {...user};
