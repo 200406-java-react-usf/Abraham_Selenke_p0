25ec4aea7fa0d2b014a117f850b174ed4fa15aeb
Abraham Selenke <slinky@Abraham.local>
2020-05-01 23:29:55 -0600

    Started but is not finished, realized that user-repo has to be completely finish before starting repo :(

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -1,4 +1,22 @@
 import url from 'url';
 import express from 'express';
 import AppConfig from '../config/app';
+import { isEmptyObject } from '../util/validator'
+import { ParsedUrlQuery } from 'querystring';
+import { adminGuard } from '../middleware/auth-middleware';
 
+export const UserRouter = express.Router();
+
+const userService = AppConfig.userService;
+
+UserRouter.get('', adminGuard, async (req, resp) => {
+    try{
+        
+        let reqURL = url.parse(req.url, true);
+
+        if(!isEmptyObject<ParsedUrlQuery> (reqURL.query)) {
+            //missing more information in the user repo
+            //finishing repo before I start routers
+        }
+    }
+})
