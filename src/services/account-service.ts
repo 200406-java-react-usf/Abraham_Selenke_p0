d914bb579dfc8a955d953ecc3b0aaf091cfe2dc1
Abraham Selenke <slinky@Abraham.local>
2020-05-05 23:07:20 -0600

    Fixed minor errors in account service

--- src/services/account-service.ts
+++ src/services/account-service.ts
@@ -3,12 +3,22 @@ import { AccountRepository } from "../repos/account-repo";
 import { ResourceNotFoundError, BadRequestError, AuthenticationError, ResourcePersistenceError } from "../errors/errors";
 import { isValidMoney, isValidString, isPropertyOf, isValidId, isValidObject, isEmptyObject } from "../util/validator";
 
+/**
+ * Services of an account
+ */
 export class AccountService {
 
+    /**
+     * 
+     * @param accountRepo Calls the account repository
+     */
     constructor(private accountRepo: AccountRepository) {
         this.accountRepo = accountRepo;
     }
 
+    /**
+     * Validates all accounts and removes time created
+     */
     async getAllAccounts(): Promise<Account[]> {
 
         let accounts = await this.accountRepo.getAll();
@@ -20,6 +30,10 @@ export class AccountService {
         return accounts.map(this.removeTime);
     }
 
+    /**
+     * 
+     * @param id The id of the account
+     */
     async getAccountById(id: number): Promise<Account> {
 
         if (!isValidId(id)) {
@@ -28,7 +42,6 @@ export class AccountService {
 
         let account = await this.accountRepo.getById(id);
 
-        //Need to test line 33
         if(isEmptyObject(account)) {
             throw new ResourceNotFoundError();
         }
@@ -36,6 +49,10 @@ export class AccountService {
         return this.removeTime(account);
     }
 
+    /**
+     * 
+     * @param newAccount Creates a new account and Validates all information
+     */
     async addNewAccount(newAccount: Account): Promise<Account> {
         try{
             if(!isValidMoney(newAccount.balance)) {
@@ -54,16 +71,19 @@ export class AccountService {
         }
     }
 
+    /**
+     * 
+     * @param updateAccount Updates a current account infromations
+     */
     async updateAccount(updateAccount: Account): Promise<boolean> {
 
         try {
-            //Need to test line 61
             if(!isValidMoney(updateAccount.balance)) {
-                throw new BadRequestError('Invalid property value found in balance.')
+                throw new BadRequestError()
             }
-            //Need to test line 65
+
             if(!isValidString(updateAccount.accountType)) {
-                throw new BadRequestError('Invalid property value in account type.')
+                throw new BadRequestError()
             }
 
             return await this.accountRepo.update(updateAccount);
@@ -72,23 +92,21 @@ export class AccountService {
         }
     }
 
-    async deleteById(id: number): Promise<boolean> {
+    /**
+     * 
+     * @param id The id of account that will get deleted
+     */
+    async deleteById(id: Object): Promise<boolean> {
         
-        //Need to test lines 77-98
         try { 
             let keys = Object.keys(id);
             
             if(!keys.every(key => isPropertyOf(key, Account))) {
-                throw new BadRequestError("id is the error");
+                throw new BadRequestError();
             }
             
             let key = keys[0];
 		    let accountId = +id[key];
-        
-
-            if(!keys.every(key => isPropertyOf(key, Account))) {
-                throw new BadRequestError();
-            }
             
 		    if (!isValidId(accountId)) {
                 throw new BadRequestError();
@@ -102,6 +120,10 @@ export class AccountService {
 
     }
 
+    /**
+     * 
+     * @param account removes time created of account
+     */
     private removeTime(account: Account): Account {
         if(!account || !account.time) return account;
         let acc = {...account};
